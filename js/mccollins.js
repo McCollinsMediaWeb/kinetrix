1cb6875945218ec37794fb4abb98440de3d0a43b
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2023-02-26 09:06:32 +0530

    ijas

--- js/mccollins.js
+++ js/mccollins.js
@@ -23,8 +23,8 @@ $(document).ready(
 
           jQuery(".popup2-trigger").click(function () {
             jQuery("body").toggleClass("Popup2active");
-            jQuery(".p2HeadText").text($(this).children("ServiceItemT2").html());
-            jQuery(".p2mainContent").text($(this).children("ServiceItemT3").html());
+            jQuery(".p2HeadText").text($(this).children(".ServiceItemT2").html());
+            jQuery(".p2mainContent").text($(this).children(".ServiceItemT3").html());
           });
           jQuery(".OverLay2").click(function () {
             jQuery("body").toggleClass("Popup2active");
